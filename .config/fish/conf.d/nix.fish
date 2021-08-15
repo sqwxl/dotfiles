594df9d2244444ed8be50d836401f866b20f97a9
Nicolas Epstein <nilueps@gmail.com>
2021-08-15 18:21:33 -0400

    added folders

---
+++ .config/fish/conf.d/nix.fish
@@ -0,0 +1,8 @@
+# nix environment setup
+# translated from '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+set -gx NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
+set -gx NIX_SSL_CERT_FILE /etc/pki/tls/certs/ca-bundle.crt # Fedora specific
+set -gx NIX_PATH nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixpkgs:/nix/var/nix/profiles/per-user/root/channels
+fish_add_path $HOME/.nix-profile/bin
+fish_add_path /nix/var/nix/profiles/default/bin
+
