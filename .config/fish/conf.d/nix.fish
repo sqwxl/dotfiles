f382d3e4a75614cf258701d9a021bc0c3f1c271e
Nicolas Epstein <nilueps@gmail.com>
2021-08-19 10:28:08 -0400

    fix syntax error

--- .config/fish/conf.d/nix.fish
+++ .config/fish/conf.d/nix.fish
@@ -6,4 +6,4 @@ if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 	set -gx NIX_PATH nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixpkgs:/nix/var/nix/profiles/per-user/root/channels
 	fish_add_path $HOME/.nix-profile/bin
 	fish_add_path /nix/var/nix/profiles/default/bin
-done
+end
