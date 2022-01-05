dea7988d30a0f9870d50e5bf06725b28f86f0f9b
Nicolas Epstein <nilueps@gmail.com>
2022-01-05 09:55:20 -0500

    lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -12,9 +12,9 @@ vim.cmd [[
   augroup end
 ]]
 
-local use = require'packer'.use
+local use = require 'packer'.use
 
-require'packer'.startup(function()
+require 'packer'.startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
 
   -- Enhancements
@@ -25,65 +25,110 @@ require'packer'.startup(function()
   use 'andymass/vim-matchup'
   use 'ludovicchabant/vim-gutentags'
   use {
-      'numToStr/Comment.nvim',
-      config = function()
-          require'Comment'.setup()
-      end
+    'numToStr/Comment.nvim',
+    config = function()
+      require 'Comment'.setup()
+    end
   }
+
   use {
     'windwp/nvim-autopairs',
     config = function()
-      require'nvim-autopairs'.setup {
-      check_ts = true,
+      require 'nvim-autopairs'.setup {
+        check_ts = true,
+        disable_in_macro = true,
       }
     end
   }
+
   use 'windwp/nvim-ts-autotag'
+
   use 'ggandor/lightspeed.nvim'
   -- use 'github/copilot.vim'
 
-  -- Look & feel
+  -- Look & fel
   use 'morhetz/gruvbox'
   use 'folke/lsp-colors.nvim'
 
   -- Tools
   use {
       'kyazdani42/nvim-tree.lua',
-      requires = {
-        'kyazdani42/nvim-web-devicons',
-      },
-      config = function() require'nvim-tree'.setup {} end
+      requires = { 'kyazdani42/nvim-web-devicons' },
+      config = function() require 'nvim-tree'.setup {
+          hijack_cursor = true,
+          diagnostics = {
+            enable = true,
+          },
+          view = {
+            signcolumn = "no",
+          },
+        }
+      end
   }
 
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' } }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
   use {
     'nvim-lualine/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons'},
-    config = function() require'lualine'.setup {} end
+    config = function()
+      require 'lualine'.setup {
+        tabline = {
+          lualine_a = {
+            'buffers'
+          }
+        },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          },
+          lualine_x = { 'filetype' },
+        },
+        inactive_sections = { lualine_x = {} },
+        extensions = { 'quickfix', 'toggleterm', 'nvim-tree' },
+      }
+    end
+  }
+
+  use {
+    'akinsho/toggleterm.nvim',
+    config = function ()
+      require 'toggleterm'.setup {
+        open_mapping = '<A-Space>',
+        shade_terminals = false,
+      }
+    end
   }
   use {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-        require'indent_blankline'.setup {
-          char = '┊',
-          filetype_exclude = { 'help', 'packer' },
-          buftype_exclude = { 'terminal', 'nofile' },
-          show_trailing_blankline_indent = false,
-        }
+      require 'indent_blankline'.setup {
+        filetype_exclude = { 'help', 'packer' },
+        buftype_exclude = { 'terminal', 'nofile' },
+        show_trailing_blankline_indent = false,
+      }
     end
   }
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-  config = function() require'gitsigns'.setup {
-    signs = {
-      add = { hl = 'GitGutterAdd', text = '+' },
-      change = { hl = 'GitGutterChange', text = '~' },
-      delete = { hl = 'GitGutterDelete', text = '_' },
-      topdelete = { hl = 'GitGutterDelete', text = '‾' },
-      changedelete = { hl = 'GitGutterChange', text = '~' },
-    },
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require 'gitsigns'.setup {
+        signs = {
+          add = { hl = 'GitGutterAdd', text = '+' },
+          change = { hl = 'GitGutterChange', text = '~' },
+          delete = { hl = 'GitGutterDelete', text = '_' },
+          topdelete = { hl = 'GitGutterDelete', text = '‾' },
+          changedelete = { hl = 'GitGutterChange', text = '~' },
+        },
+      }
+    end
   }
-      end}
 
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
@@ -102,234 +147,7 @@ require'packer'.startup(function()
 
 end)
 
---Map blankline
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "bash",
-    "css",
-    "dockerfile",
-    "fish",
-    "glsl",
-    "go",
-    "html",
-    "javascript",
-    "json",
-    "lua",
-    "make",
-    "markdown",
-    "nix",
-    "pug",
-    "python",
-    "rust",
-    "scss",
-    "svelte",
-    "tsx",
-    "typescript",
-    "vim",
-    "yaml",
-  },
-  highlight = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
-    },
-  },
-  indent = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-  },
-  matchup = {
-    enable = true,
-  },
-  autotag = {
-    enable = true,
-  }
-}
-
--- Telescope
-require'telescope'.setup {
-  defaults = {
-    initial_mode = 'normal',
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
-require'telescope'.load_extension 'fzf'
-
-local lspconfig = require'lspconfig'
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local cmp = require('cmp')
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-D>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-U>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-E>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
-      end
-    end, { "i", "s" }),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-  },{
-    { name = 'buffer' },
-  }),
-})
-
-cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
-
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' },
-  }, {
-    { name = 'cmdline' },
-  }),
-})
-
-local cmp_autopairs = require'nvim-autopairs.completion.cmp'
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done())
-
-local capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-local servers = {
-  "eslint",
-  "jsonls",
-  "cssls",
-  "vimls",
-  "gopls",
-  "bashls",
-  "pylsp",
-  "rnix",
-  "tsserver",
-  "html",
-}
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = 150,
-    }
-  })
-end
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-lspconfig.sumneko_lua.setup({
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = {'vim'},
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
-})
-
-require'rust-tools'.setup {
-  tools = {
-    autoSetHints = true,
-    hover_with_actions = true,
-    inlay_hints = {
-      show_parameter_hints = false,
-      parameter_hints_prefix = "",
-      other_hints_prefix = "",
-    },
-  },
-  server = {
-    capabilities = capabilities,
-    settings = {
-      ["rust-analyzer"] = {
-        checkOnSave = {
-          command = "clippy"
-        },
-      },
-    },
-  },
-}
+require 'config.telescope'
+require 'config.treesitter'
+-- require 'config.autopairs'
+require 'config.lsp'
--- .vimrc
+++ .vimrc
@@ -1,1 +1,63 @@
-.config/nvim/init.vim
+filetype plugin indent on
+syntax enable
+set noswapfile
+set hidden
+set undofile undodir=~/.vimdid
+set clipboard+=unnamedplus
+set mouse=a
+
+set updatetime=300
+set ttimeoutlen=10
+set lazyredraw
+
+set splitright splitbelow
+
+set completeopt=menu,menuone,noselect
+set shortmess+=c
+
+set termguicolors
+autocmd vimenter * ++nested colorscheme gruvbox
+
+set number relativenumber numberwidth=1
+set signcolumn=yes
+set colorcolumn=100
+set scrolloff=2
+set noshowmode
+
+set nowrap
+set linebreak breakindent showbreak=﬌\ 
+
+set shiftwidth=2 tabstop=2 softtabstop=2 expandtab autoindent smartindent
+set listchars=tab:→·,nbsp:·,extends:»,precedes:«,trail:~
+
+set ignorecase smartcase
+set gdefault
+
+set backspace=indent,eol,start
+
+set diffopt+=vertical
+
+set guioptions-=T " Remove toolbar
+set guifont=JetBrainsMono_Nerd_Font_Mono:h10
+set guicursor+=a:blinkwait200-blinkoff125-blinkon150-Cursor/lCursor
+if has('ide') " jetbrains
+  set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+endif
+
+if executable('rg')
+  set grepprg=rg\ --vimgrep\ --smart-case
+  set grepformat=%f:%l:%c:%m
+endif
+
+ " Toggle 'relativenumber' on insert.
+augroup numbertoggle
+  autocmd!
+  autocmd InsertEnter,BufLeave,WinLeave,FocusLost * nested
+        \ if &l:number && empty(&buftype) |
+        \ setlocal norelativenumber |
+        \ endif
+  autocmd InsertLeave,BufEnter,WinEnter,FocusGained * nested
+        \ if &l:number && mode() != 'i' && empty(&buftype) |
+        \ setlocal relativenumber |
+        \ endif
+augroup END
