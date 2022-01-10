441f3631b9a76281c0bc07ac6d1063a4842ef7fb
Nicolas Epstein <nilueps@gmail.com>
2022-01-10 11:22:28 -0500

    ok

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,89 +1,91 @@
 local map = vim.api.nvim_set_keymap
-local noremap = { noremap = true }
-local noremap_silent = { noremap = true, silent = true }
+local noremap = {noremap = true}
+local noremap_silent = {noremap = true, silent = true}
 
---Remap space as leader key
+-- Remap space as leader key
 map('', '<Space>', '<Nop>', noremap_silent)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
---Remap Esc
+-- Remap Esc
 map('i', 'fj', '<Esc>', noremap)
 map('i', 'jf', '<Esc>', noremap)
 map('c', 'fj', '<Esc>', noremap)
 map('c', 'jf', '<Esc>', noremap)
 map('t', '<Esc>', '<C-\\><C-N>', noremap)
 
---Remap for dealing with word wrap
-map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
-map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+-- Remap for dealing with word wrap
+map('n', 'k', "v:count == 0 ? 'gk' : 'k'", {noremap = true, expr = true, silent = true})
+map('n', 'j', "v:count == 0 ? 'gj' : 'j'", {noremap = true, expr = true, silent = true})
 
---Undo breaks before deletes
+-- Undo breaks before deletes
 map('i', '<C-U>', '<C-G>u<C-U>', noremap)
 map('i', '<C-W>', '<C-G>u<C-W>', noremap)
 
---Ergonomics
+-- Ergonomics
 map('n', ';', ':', noremap)
 
-map('n', 'H', "^", noremap)
-map('n', 'L', "$", noremap)
-map('v', 'H', "^", noremap)
-map('v', 'L', "$", noremap)
+map('n', 'H', '^', noremap)
+map('n', 'L', '$', noremap)
+map('v', 'H', '^', noremap)
+map('v', 'L', '$', noremap)
 
-map('n', 'Y', "y$", noremap)
-map('n', 'Q', "@q", noremap)
+map('n', 'Y', 'y$', noremap)
+map('n', 'Q', '@q', noremap)
+map('n', 'D', 'd$', noremap)
 
 map('n', '<C-H>', '<C-W>h', noremap)
 map('n', '<C-J>', '<C-W>j', noremap)
 map('n', '<C-K>', '<C-W>k', noremap)
 map('n', '<C-L>', '<C-W>l', noremap)
 
---Move lines
+-- Move lines
 map('n', '<A-j>', ':m .+1<CR>==', noremap)
 map('n', '<A-k>', ':m .-2<CR>==', noremap)
 map('i', '<A-j>', '<Esc>:m .+1<CR>==gi', noremap)
 map('i', '<A-k>', '<Esc>:m .-2<CR>==gi', noremap)
 map('v', '<A-j>', ':m \'>+1<CR>gv=gv', noremap)
 map('v', '<A-k>', ':m \'<-2<CR>gv=gv', noremap)
-
 map('n', '<Tab>', '>>', noremap)
 map('v', '<Tab>', '>', noremap)
 map('n', '<S-Tab>', '<<', noremap)
 map('v', '<S-Tab>', '<', noremap)
 
---Search
+-- Search
 map('n', '?', [[?\v]], noremap)
 map('n', '/', [[/\v]], noremap)
 map('c', '%s/', '%sm/', noremap)
 map('n', '<leader>h', ':noh<CR>', noremap_silent)
 
---Show/hide hidden characters
+-- Show/hide hidden characters
 map('n', '<leader>,', ':set invlist<CR>', noremap_silent)
 
---Buffers
+-- Buffers
 map('n', '<leader><space>', '<C-^>', noremap)
 map('n', '<leader>n', ':bnext<CR>', noremap_silent)
 map('n', '<leader>p', ':bprevious<CR>', noremap_silent)
 map('n', '<C-X>', ':bwipeout<CR>', noremap_silent)
 
---Write/quit
+-- Write/quit
 map('n', '<leader>w', ':w<CR>', noremap)
 map('n', '<C-Q>', ':confirm quitall<CR>', noremap)
+map('n', '<C-,>', ':tabnew<CR> :e ~/.config/nvim/init.lua<CR>', noremap)
 
 map('n', '<leader>t', ':NvimTreeToggle<CR>', noremap)
 
+map('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], noremap_silent)
+map('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').git_files()<CR>]], noremap_silent)
 map('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], noremap_silent)
-map('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], noremap_silent)
+map('n', '<leader>fj', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], noremap_silent)
+map('n', '<leader>fs', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], noremap_silent)
 map('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], noremap_silent)
 map('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], noremap_silent)
 map('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], noremap_silent)
-map('n', '<leader>ss', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], noremap_silent)
-map('n', '<leader>sl', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], noremap_silent)
 map('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], noremap_silent)
 map('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], noremap_silent)
 
---LSP
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', noremap_silent )
+-- LSP
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', noremap_silent)
 map('n', 'J', '<cmd>lua vim.lsp.buf.code_action()<CR>', noremap_silent)
 map('n', '<leader>k', '<Cmd>lua vim.lsp.buf.declaration()<CR>', noremap_silent)
 map('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', noremap_silent)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,7 +2,7 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 vim.cmd [[
@@ -12,64 +12,46 @@ vim.cmd [[
   augroup end
 ]]
 
-local use = require 'packer'.use
+local use = require'packer'.use
 
-require 'packer'.startup(function()
-  use 'wbthomason/packer.nvim' -- Package manager
+require'packer'.startup(function()
+    use 'wbthomason/packer.nvim' -- Package manager
 
-  -- Enhancements
-  use 'lbrayner/vim-rzip'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  use 'andymass/vim-matchup'
-  use 'ludovicchabant/vim-gutentags'
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require 'Comment'.setup()
-    end
-  }
+    -- Enhancements
+    use 'lbrayner/vim-rzip'
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rhubarb'
+    use 'tpope/vim-surround'
+    use 'tpope/vim-repeat'
+    use 'andymass/vim-matchup'
+    use {'ludovicchabant/vim-gutentags', disable = true}
 
-  use {
-    'windwp/nvim-autopairs',
-    config = function()
-      require 'nvim-autopairs'.setup {
-        check_ts = true,
-        disable_in_macro = true,
-      }
-    end
-  }
+    use {'numToStr/Comment.nvim', config = function() require'Comment'.setup() end}
 
-  use 'windwp/nvim-ts-autotag'
+    use {
+        'windwp/nvim-autopairs',
+        config = function() require'nvim-autopairs'.setup {check_ts = true, disable_in_macro = true} end
+    }
 
-  use 'ggandor/lightspeed.nvim'
-  -- use 'github/copilot.vim'
+    use 'windwp/nvim-ts-autotag'
 
-  -- Look & fel
-  use 'morhetz/gruvbox'
-  use 'folke/lsp-colors.nvim'
+    use 'ggandor/lightspeed.nvim'
+    -- use 'github/copilot.vim'
 
-  -- Tools
-  use {
-      'kyazdani42/nvim-tree.lua',
-      requires = { 'kyazdani42/nvim-web-devicons' },
-      config = function() require 'nvim-tree'.setup {
-          hijack_cursor = true,
-          diagnostics = {
-            enable = true,
-          },
-          view = {
-            signcolumn = "no",
-          },
-        }
-      end
-  }
+    -- Look & fel
+    use 'morhetz/gruvbox'
+    use 'folke/lsp-colors.nvim'
 
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' } }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    -- Tools
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {'kyazdani42/nvim-web-devicons'},
+        config = function()
+            require'nvim-tree'.setup {hijack_cursor = true, diagnostics = {enable = true}, view = {signcolumn = "no"}}
+        end
+    }
 
+<<<<<<< HEAD
   use {
     'nvim-lualine/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons'},
@@ -85,69 +67,65 @@ require 'packer'.startup(function()
         },
         sections = {
           lualine_c = {
-            {
               'filename',
               path = 1,
+            },
+          lualine_x = {'filetype'}
+        },
+        inactive_sections = {lualine_x = {}},
+        extensions = {'quickfix', 'toggleterm', 'nvim-tree'}
+        }
+        end
+    }
+
+    use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim'}}
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
+
+    use {
+        'akinsho/toggleterm.nvim',
+        config = function() require'toggleterm'.setup {open_mapping = '<A-Space>', shade_terminals = false} end
+    }
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require'indent_blankline'.setup {
+                filetype_exclude = {'help', 'packer'},
+                buftype_exclude = {'terminal', 'nofile'},
+                show_trailing_blankline_indent = false
             }
-          },
-          lualine_x = { 'filetype' },
-        },
-        inactive_sections = { lualine_x = {} },
-        extensions = { 'quickfix', 'toggleterm', 'nvim-tree' },
-      }
-    end
-  }
+        end
+    }
 
-  use {
-    'akinsho/toggleterm.nvim',
-    config = function ()
-      require 'toggleterm'.setup {
-        open_mapping = '<A-Space>',
-        shade_terminals = false,
-      }
-    end
-  }
-  use {
-    'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      require 'indent_blankline'.setup {
-        filetype_exclude = { 'help', 'packer' },
-        buftype_exclude = { 'terminal', 'nofile' },
-        show_trailing_blankline_indent = false,
-      }
-    end
-  }
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = {'nvim-lua/plenary.nvim'},
+        config = function()
+            require'gitsigns'.setup {
+                signs = {
+                    add = {hl = 'GitGutterAdd', text = '+'},
+                    change = {hl = 'GitGutterChange', text = '~'},
+                    delete = {hl = 'GitGutterDelete', text = '_'},
+                    topdelete = {hl = 'GitGutterDelete', text = '‾'},
+                    changedelete = {hl = 'GitGutterChange', text = '~'}
+                }
+            }
+        end
+    }
 
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require 'gitsigns'.setup {
-        signs = {
-          add = { hl = 'GitGutterAdd', text = '+' },
-          change = { hl = 'GitGutterChange', text = '~' },
-          delete = { hl = 'GitGutterDelete', text = '_' },
-          topdelete = { hl = 'GitGutterDelete', text = '‾' },
-          changedelete = { hl = 'GitGutterChange', text = '~' },
-        },
-      }
-    end
-  }
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-treesitter/nvim-treesitter-textobjects'
 
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-lua/lsp_extensions.nvim'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
 
-  use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/lsp_extensions.nvim'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-
-  use 'simrat39/rust-tools.nvim'
+    use 'simrat39/rust-tools.nvim'
 
 end)
 
