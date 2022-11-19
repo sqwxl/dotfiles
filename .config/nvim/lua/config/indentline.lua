db967e5c385ba9fe581c66bfd22e25a7ca10ebc1
Nicolas Epstein <nilueps@gmail.com>
2022-11-19 10:47:30 -0500

    clean house

--- .config/nvim/lua/config/indentline.lua
+++
@@ -1,3 +0,0 @@
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_filetype_exclude = { "lspinfo", "packer", "checkhealth", "help", "markdown", "txt", "" }
-vim.g.indentLine_faster = 1
--- .config/nvim/lua/config/telescope_setup.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.api.nvim_set_keymap
-
-local noremap_silent = { noremap = true, silent = true }
-
-map('n', '<leader>uu', [[<cmd>Telescope find_files<cr>]], noremap_silent)
-map('n', '<leader>ub', [[<cmd>Telescope buffers show_all_buffers=true<cr>]], noremap_silent)
-map('n', '<leader>ug', [[<cmd>Telescope live_grep<cr>]], noremap_silent)
-map('n', '<leader>uh', [[<cmd>Telescope help_tags<cr>]], noremap_silent)
