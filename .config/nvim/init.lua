0e6d4c56536119d1f3d6534f6ce4f612a1588375
Nicolas Epstein <nilueps@gmail.com>
2022-11-18 19:44:48 -0500

    bleh

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,191 @@
-if not vim.g.vscode then
- require('impatient')
- -- require('plugins')
+-- ensure the packer plugin manager is installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
+local packer_bootstrap = ensure_packer()
+
+require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	-- basics
+	use("tpope/vim-repeat")
+	use("tpope/vim-commentary")
+	use("tpope/vim-endwise")
+	use("tpope/vim-surround")
+	use("tpope/vim-fugitive")
+	use({
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings()
+		end
+	})
+	
+	use("ellisonleao/gruvbox.nvim")
+
+	use("neovim/nvim-lspconfig")
+
+	use({
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end
+	})
+
+	use("hrsh7th/nvim-cmp")
+	use({
+		-- cmp LSP completion
+		"hrsh7th/cmp-nvim-lsp",
+		-- cmp Snippet completion
+		"hrsh7th/cmp-vsnip",
+		-- cmp Path completion
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		after = { "hrsh7th/nvim-cmp" },
+		requires = { "hrsh7th/nvim-cmp" },
+	})
+	-- Snippet engine
+	use('hrsh7th/vim-vsnip')
+
+	use("simrat39/rust-tools.nvim")
+
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
+	use({
+	    "glepnir/lspsaga.nvim",
+	    branch = "main",
+	    config = function()
+		local saga = require("lspsaga")
+
+		saga.init_lsp_saga({
+		    -- your configuration
+		})
+	    end,
+	})
+end)
+
+-- the first run will install packer and our plugins
+if packer_bootstrap then
+  require("packer").sync()
+  return
+end
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+vim.opt.shortmess = vim.opt.shortmess + "c"
+
+vim.opt.updatetime = 100
+
+local function on_attach(client, buffer)
+	-- This callback is called when the LSP is atttached/enabled for this buffer
+	local keymap_opts = { buffer = buffer }
+	-- Code navigation and shortcuts
+	vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+	vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
+	vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
+	vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
+	vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+	vim.keymap.set("n", "<c-.>", vim.lsp.buf.code_action, keymap_opts)
+
+	-- Show diagnostic popup on cursor hover
+	local diag_float_grp = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
+	vim.api.nvim_create_autocmd("CursorHold", {
+	  callback = function()
+	   vim.diagnostic.open_float(nil, { focusable = false })
+	  end,
+	  group = diag_float_grp,
+	})
+	-- have a fixed column for the diagnostics to appear in
+	-- this removes the jitter when warnings/errors flow in
+	vim.wo.signcolumn = "yes"
+
+	-- Goto previous/next diagnostic warning/error
+	vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, keymap_opts)
+	vim.keymap.set("n", "g]", vim.diagnostic.goto_next, keymap_opts)
+end
+
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+local opts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+require("rust-tools").setup(opts)
+
+-- Setup Completion
+-- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
+local cmp = require("cmp")
+cmp.setup({
+  preselect = cmp.PreselectMode.None,
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  },
+
+  -- Installed sources
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+})
