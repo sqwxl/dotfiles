6f5da5c91bb6a619116ba1e5e6d0b456eaa5e6c6
Nicolas Epstein <nicolas.epstein@qc.croixbleue.ca>
2023-05-25 16:59:38 -0400

    plipli

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -103,3 +103,10 @@ if type -q direnv
 end
 
 source "$HOME/.config/fish/functions/__auto_source_venv.fish"
+
+# fish_default_key_bindings -M insert
+# Then execute the vi-bindings so they take precedence when there's a conflict.
+# Without --no-erase fish_vi_key_bindings will default to
+# resetting all bindings.
+# The argument specifies the initial mode (insert, "default" or visual).
+# fish_vi_key_bindings --no-erase insert
