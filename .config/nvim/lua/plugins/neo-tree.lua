bdc791b24ad1e20fdc5dfe5f230211f55a772bc2
Nicolas Epstein <nilueps@gmail.com>
2023-08-27 11:21:37 -0400

    bump neo-tree version

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -2,7 +2,7 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
+    branch = "v3.x",
     opts = {
       window = {
         position = "right",
