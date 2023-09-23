57044006a5779ec2bc79297ce574ff2a4fe8a5a8
Nicolas Epstein <nilueps@gmail.com>
2023-09-23 13:24:55 -0400

    blip

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,18 +2,18 @@
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
   callback = function()
-    vim.opt_local.number = false;
+    vim.opt_local.number = false
     vim.opt_local.signcolumn = "no"
-    vim.cmd('startinsert')
-  end
+    vim.cmd("startinsert")
+  end,
 })
 
 -- change to terminal mode when entering terminal window
 vim.api.nvim_create_autocmd("WinEnter", {
   pattern = "term://*",
   callback = function()
-    vim.cmd('startinsert')
-  end
+    vim.cmd("startinsert")
+  end,
 })
 
 -- move help window to the left side and resize to 80 columns
@@ -21,15 +21,19 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = "*.txt",
   callback = function()
     if vim.o.filetype == "help" then
+      vim.opt_local.signcolumn = "no"
+      vim.opt_local.foldcolumn = "0"
+      vim.opt_local.colorcolumn = ""
+      vim.opt_local.wrap = true
       vim.cmd([[ wincmd H | vertical resize 80 ]])
     end
-  end
+  end,
 })
 
 -- use insert mode when entering commit edit
 vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = "*COMMIT_EDITMSG",
   callback = function()
-    vim.cmd('startinsert')
-  end
+    vim.cmd("startinsert")
+  end,
 })
