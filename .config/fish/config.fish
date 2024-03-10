d9a24080213495ffd0df8a98a968de3a8b6e329b
Nicolas Epstein <nilueps@gmail.com>
2024-03-09 19:34:04 -0500

    brew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -126,3 +126,13 @@ end
 # resetting all bindings.
 # The argument specifies the initial mode (insert, "default" or visual).
 # fish_vi_key_bindings --no-erase insert
+
+if type -q brew
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+end
