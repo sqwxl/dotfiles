80c3d00698402c23734fb3e58175c14a5f9e5924
Nicolas Epstein <nilueps@gmail.com>
2024-06-03 08:39:24 -0400

    shark

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,72 @@
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-abbr --add s sudo
-abbr --add g git
-abbr --add n nvim
-abbr --add se sudoedit
+abbr -a s sudo
+abbr -a g git
+abbr -a n nvim
+abbr -a se sudoedit
 
-abbr --add gr "cd (git rev-parse --show-toplevel 2>/dev/null || $PWD)"
-abbr --add gs 'git status'
+abbr -a ga 'git add'
+abbr -a gaa 'git add --all'
+abbr -a gai 'git add --interactive'
+abbr -a gap 'git add --patch'
+abbr -a gau 'git add --update'
+abbr -a gb 'git branch'
+abbr -a gba 'git branch --all'
+abbr -a gc 'git commit'
+abbr -a gca 'git commit --all'
+abbr -a gcam 'git commit --all --message'
+abbr -a gcan 'git commit --amend --no-edit'
+abbr -a gcana 'git commit --amend --no-edit --all'
+abbr -a gcfix 'git commit --fixup'
+abbr -a gcl 'git clean --interactive -d'
+abbr -a gco 'git checkout'
+abbr -a gco 'git commit'
+abbr -a gcp 'git cherry-pick'
+abbr -a gcpa 'git cherry-pick --abort'
+abbr -a gcpc 'git cherry-pick --continue'
+abbr -a gcsqu 'git commit --squash'
+abbr -a gd 'git diff'
+abbr -a gds 'git diff --staged'
+abbr -a gdup 'git diff @\{upstream\}'
+abbr -a gf 'git fetch'
+abbr -a gl 'git log --decorate --pretty=oneline --abbrev-commit'
+abbr -a glg 'git log --graph --decorate --pretty=oneline --abbrev-commit'
+abbr -a glga 'git log --graph --decorate --pretty=oneline --abbrev-commit --all'
+abbr -a gm 'git merge'
+abbr -a gma 'git merge --abort'
+abbr -a gmc 'git merge --continue'
+abbr -a gmt 'git mergetool'
+abbr -a gp 'git push'
+abbr -a gpf 'git push --force-with-lease'
+abbr -a gpl 'git pull'
+abbr -a gplr 'git pull --rebase'
+abbr -a gplra 'git pull --rebase --autostash'
+abbr -a gpristine 'git reset --hard && git clean --force -d -x'
+abbr -a gr "cd (git rev-parse --show-toplevel 2>/dev/null || $PWD)"
+abbr -a gr 'git reset'
+abbr -a grb 'git rebase'
+abbr -a grba 'git rebase --abort'
+abbr -a grbc 'git rebase --continue'
+abbr -a grbi 'git rebase --interactive'
+abbr -a grh 'git reset --hard'
+abbr -a grk 'git reset --keep'
+abbr -a grl 'git reflog'
+abbr -a grm 'git rm'
+abbr -a grmc 'git rm --cached'
+abbr -a groh 'git reset --hard @{upstream}'
+abbr -a grs 'git reset --soft'
+abbr -a grv 'git remote -v'
+abbr -a gs 'git status --short --branch'
+abbr -a gsta 'git stash --include-untracked'
+abbr -a gstk 'git stash --keep-index'
+abbr -a gstl 'git stash list'
+abbr -a gstp 'git stash pop'
+abbr -a gsw 'git switch'
+abbr -a gswc 'git switch -c'
+abbr -a gwta 'git worktree add'
+abbr -a gwtl 'git worktree list'
+abbr -a gwtrm 'git worktree remove'
 
 alias cfginit "git init --bare $HOME/.dotfiles && git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote add origin && echo .dotfiles >> $HOME/.gitignore"
 alias cfg "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
