3c94779f0da7d4627a310b13677d409112fc1ae1
Nicolas Epstein <nilueps@gmail.com>
2024-09-26 09:47:15 -0400

    halibut

--- .config/fish/conf.d/xkcd.fish
+++ .config/fish/conf.d/xkcd.fish
@@ -1,10 +1,10 @@
 if not type -q magick
     exit
 end
-set -l day (date +%j)
 if not set -q xkcd_day
     set -U xkcd_day -1
 end
+set -l day (date +%j) # 0-365
 if test $day != $xkcd_day
     set xkcd_day $day
     curl -s (curl -s https://xkcd.com/info.0.json | jq -r '.img') | magick png:- -border 4% sixel:-
