95997b2db72bc63cf3a3fefbf3e51303de1bf462
Nicolas Epstein <nilueps@gmail.com>
2024-10-21 08:24:10 -0400

    Update .config/nvim/lua/plugins/chezmoi.lua

--- private_dot_config/nvim/lua/plugins/chezmoi.lua
+++ private_dot_config/nvim/lua/plugins/chezmoi.lua
@@ -61,8 +61,8 @@ return {
       vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
         callback = function()
-          vim.schedule(require("chezmoi.commands.__edit").watch)
           vim.print("hello")
+          vim.schedule(require("chezmoi.commands.__edit").watch)
         end,
       })
     end,
