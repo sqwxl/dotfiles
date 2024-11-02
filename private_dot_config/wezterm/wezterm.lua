cfd365a6d749e4ab98bae7f2af0dbcd79350fde3
Nicolas Epstein <nilueps@gmail.com>
2024-11-02 10:27:48 -0400

    Update .config/foot/foot.ini Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.default_prog = { "/usr/bin/fish" }
+
 config.font = wezterm.font("FantasqueSansM Nerd Font Mono")
 config.font_size = 12
 
@@ -10,8 +12,8 @@ config.color_scheme = "GruvboxDark"
 config.integrated_title_button_style = "Gnome"
 config.integrated_title_buttons = { "Close" }
 config.use_fancy_tab_bar = false
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+-- config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
