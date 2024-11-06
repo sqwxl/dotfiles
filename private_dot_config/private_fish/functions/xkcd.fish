7a3152626087880d570d42387032ec5ef737a2f3
Nicolas Epstein <nilueps@gmail.com>
2024-11-05 20:08:46 -0500

    Update .config/fish/functions/xkcd.fish

--- private_dot_config/private_fish/functions/xkcd.fish
+++ private_dot_config/private_fish/functions/xkcd.fish
@@ -2,5 +2,6 @@ function xkcd
     if not type -q magick
         return
     end
-    curl -s https://xkcd.com/info.0.json | jq -r '.img' | xargs curl -s | magick png:- -bordercolor "#282828" -border 5% sixel:-
+    curl -s https://xkcd.com/info.0.json | jq -r '.img' | xargs curl -s | magick png:- sixel:-
+    # curl -s https://xkcd.com/info.0.json | jq -r '.img' | xargs curl -s | magick png:- -bordercolor "#282828" -border 5% sixel:-
 end
