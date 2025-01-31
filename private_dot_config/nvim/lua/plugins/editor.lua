23786cbcc20401f84718dd7aa74d52c931e787c8
Nicolas Epstein <nilueps@gmail.com>
2025-01-31 09:59:20 -0500

    Update en.utf-8.add Update .config/nvim/lua/plugins/editor.lua

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -65,7 +65,7 @@ return {
       selection_chars = "aoeuidhtnsqjkxbmwvz",
       filter_rules = {
         bo = {
-          filetype = { "neo-tree", "neo-tree-popup", "notify", "noice", "snacks_notif" },
+          filetype = { "neo-tree", "neo-tree-popup", "notify", "noice", "snacks_notif", "aerial" },
           buftype = { "terminal" },
         },
       },
