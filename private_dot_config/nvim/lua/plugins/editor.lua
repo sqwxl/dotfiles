a7f28b8961c02373a48e676e8a8936a4088ccd77
Nicolas Epstein <nilueps@gmail.com>
2025-02-25 10:38:28 -0500

    Update .config/nvim/lua/plugins/editor.lua

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -2,15 +2,6 @@ return {
 
   {
     "folke/snacks.nvim",
-    opts = {
-      picker = {
-        sources = {
-          explorer = {
-            auto_close = true,
-          },
-        },
-      },
-    },
     keys = {
       {
         "<Bslash>",
