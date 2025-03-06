c4087b632a7a83a1cd1d219d414221908f30d7e9
Nicolas Epstein <nilueps@gmail.com>
2025-03-05 20:38:52 -0500

    Update lazy-lock.json Update .config/nvim/lua/plugins/lsp.lua

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,79 +1,82 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
+    opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
       vim.list_extend(keys, {
         { "<Leader>r", vim.lsp.buf.rename, desc = "Rename" },
       })
-
-      return vim.tbl_deep_extend("force", opts, {
-        inlay_hints = { enabled = false },
-        diagnostics = {
-          update_in_insert = false,
-        },
-        servers = {
-          ada_ls = {},
-          basedpyright = {
-            settings = {
-              basedpyright = {
-                disableOrganizeImports = true, -- Using Ruff
-                disableTaggedHints = true, -- Using Ruff
-                analysis = {
-                  autoImportCompletions = true,
-                  diagnosticSeverityOverrides = {
-                    reportUndefinedVariable = "none",
-                  },
-                },
-              },
-            },
-          },
-          ruff = {
-            settings = {
-              lineLength = { 120 },
-            },
-          },
-          bashls = {},
-          html = {
-            filetypes = { "html", "htmldjango" },
-          },
-          harper_ls = {
-            root_dir = function(fname)
-              -- Only run Harper in Normcore projects
-              if string.match(fname, "^.*[nN]ormcore.*$") == nil then
-                return nil
-              end
-
-              return require("lspconfig").harper_ls.config_def.default_config.root_dir(fname)
-            end,
-            single_file_support = false, -- If this is true, then the rood_dir function above will have no effect
-            settings = {
-              ["harper-ls"] = {
-                userDictPath = vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
-                codeActions = { forceStable = true },
-                linters = {
-                  avoid_curses = false,
-                },
-              },
-            },
-          },
-          htmx = {
-            filetypes = { "html", "htmldjango" },
-          },
-          biome = {
-            autostart = false,
-          },
-          lua_ls = {
-            settings = {
-              Lua = {
-                codelens = { enabled = false },
-              },
-            },
-          },
-        },
-      })
     end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      diagnostics = {
+        update_in_insert = false,
+      },
+      servers = {
+        ada_ls = {},
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              disableOrganizeImports = true, -- Using Ruff
+              disableTaggedHints = true, -- Using Ruff
+              analysis = {
+                autoImportCompletions = true,
+                diagnosticSeverityOverrides = {
+                  reportUndefinedVariable = "none",
+                },
+              },
+            },
+          },
+        },
+        ruff = {
+          settings = {
+            lineLength = { 120 },
+          },
+        },
+        bashls = {},
+        html = {
+          filetypes = { "html", "htmldjango" },
+        },
+        harper_ls = {
+          root_dir = function(fname)
+            -- Only run Harper in Normcore projects
+            if string.match(fname, "^.*[nN]ormcore.*$") == nil then
+              return nil
+            end
+
+            return require("lspconfig").harper_ls.config_def.default_config.root_dir(fname)
+          end,
+          single_file_support = false, -- If this is true, then the rood_dir function above will have no effect
+          settings = {
+            ["harper-ls"] = {
+              userDictPath = vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
+              codeActions = { forceStable = true },
+              linters = {
+                avoid_curses = false,
+              },
+            },
+          },
+        },
+        htmx = {
+          filetypes = { "html", "htmldjango" },
+        },
+        biome = {
+          autostart = false,
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              codelens = { enabled = false },
+            },
+          },
+        },
+      },
+    },
   },
 
   {
@@ -89,6 +92,7 @@ return {
         "rustywind",
         "shellcheck",
         "shfmt",
+        "sqlfluff",
         "stylua",
       },
     },
