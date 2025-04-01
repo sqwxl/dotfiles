4bc94f39d86d79af8bd8ac2f47829f32cee3368d
Nicolas Epstein <nilueps@gmail.com>
2025-03-31 22:01:01 -0400

    Remove .config/nvim/after/compiler/pdc.lua Remove .config/nvim/after/ftplugin/lua.lua Remove .config/nvim/lua/config/autocmds.lua Remove .config/nvim/lua/config/commands.lua Remove .config/nvim/lua/config/keymaps.lua Remove .config/nvim/lua/config/options.lua Remove .config/nvim/lua/lsp/config.lua Remove .config/nvim/lua/lsp/init.lua Remove .config/nvim/lua/plugins/ai.lua Remove .config/nvim/lua/plugins/coding.lua Remove .config/nvim/lua/plugins/colorscheme.lua Remove .config/nvim/lua/plugins/dap.lua Remove .config/nvim/lua/plugins/editor.lua Remove .config/nvim/lua/plugins/formatting.lua Remove .config/nvim/lua/plugins/linting.lua Remove .config/nvim/lua/plugins/lsp/config.lua Remove .config/nvim/lua/plugins/lsp/init.lua Remove .config/nvim/lua/plugins/misc.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/lua/plugins/ui.lua Remove .config/nvim/lua/plugins/util.lua Remove .config/nvim/lazyvim.json

--- private_dot_config/nvim/after/ftplugin/lua.lua
+++
@@ -1,9 +0,0 @@
-function exists(file)
-  local f = io.open(file, "r")
-  return f ~= nil and io.close(f)
-end
-
--- Playdate-specific
-if exists(vim.fn.getcwd() .. "pdxinfo") then
-  vim.cmd("compiler pdc")
-end
--- private_dot_config/nvim/lua/plugins/misc.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "eandrju/cellular-automaton.nvim",
-    lazy = true,
-    keys = { { "<Leader>fk", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain!" } },
-  },
-}
