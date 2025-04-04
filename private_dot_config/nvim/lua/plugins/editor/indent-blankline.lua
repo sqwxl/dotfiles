484ef52f197286d24a8d437e09483b898627dbd3
Nicolas Epstein <nilueps@gmail.com>
2025-04-04 15:46:16 -0400

    Update lazy-lock.json Update .config/nvim/lua/plugins/editor/indent-blankline.lua

--- private_dot_config/nvim/lua/plugins/editor/indent-blankline.lua
+++ private_dot_config/nvim/lua/plugins/editor/indent-blankline.lua
@@ -1,7 +1,14 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
+    -- For setting shiftwidth and tabstop automatically.
+    dependencies = "tpope/vim-sleuth",
     ---@module "ibl"
     ---@type ibl.config
-    opts = {},
+    opts = {
+        scope = {
+            show_start = false,
+            show_end = false
+        }
+    },
 }
