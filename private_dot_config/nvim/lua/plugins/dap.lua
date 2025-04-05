8d491cb9e481b82ef064f56a69a501d8f09b7e06
Nicolas Epstein <nilueps@gmail.com>
2025-04-05 18:47:33 -0400

    Update .config/nvim/lua/plugins/dap.lua

--- private_dot_config/nvim/lua/plugins/dap.lua
+++ private_dot_config/nvim/lua/plugins/dap.lua
@@ -18,20 +18,7 @@ return {
         },
 
         config = function()
-            -- load mason-nvim-dap here, after all adapters have been setup
-            if LazyVim.has("mason-nvim-dap.nvim") then
-                require("mason-nvim-dap").setup(LazyVim.opts("mason-nvim-dap.nvim"))
-            end
-
             vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-            for name, sign in pairs(LazyVim.config.icons.dap) do
-                sign = type(sign) == "table" and sign or { sign }
-                vim.fn.sign_define(
-                    "Dap" .. name,
-                    { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-                )
-            end
 
             -- setup dap config by VsCode launch.json file
             local vscode = require("dap.ext.vscode")
