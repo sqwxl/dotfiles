a9bc0ffeb4c2342416abf82a6333c30baeec893f
Nicolas Epstein <nilueps@gmail.com>
2025-04-08 17:11:56 -0400

    Update .config/nvim/lua/config/lsp.lua

--- private_dot_config/nvim/lua/config/lsp.lua
+++ private_dot_config/nvim/lua/config/lsp.lua
@@ -109,16 +109,16 @@ local servers = {
 
 vim.lsp.inlay_hint.enable(false)
 
--- vim.diagnostic.config({
---     underline = true,
---     update_in_insert = false,
---     virtual_text = {
---         spacing = 4,
---         source = "if_many",
---         prefix = "●",
---     },
---     severity_sort = true,
--- })
+vim.diagnostic.config({
+    underline = true,
+    update_in_insert = false,
+    virtual_text = {
+        spacing = 4,
+        source = "if_many",
+        prefix = "●",
+    },
+    severity_sort = true,
+})
 
 for server, config in pairs(servers) do
     config.capabilities = vim.tbl_deep_extend(
@@ -133,9 +133,9 @@ end
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("sqwxl.lsp", {}),
     callback = function(args)
-        -- local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        --
-        -- -- Don't use LSP for syntax highlighting
-        -- client.server_capabilities.semanticTokensProvider = nil
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+        -- Don't use LSP for syntax highlighting
+        client.server_capabilities.semanticTokensProvider = nil
     end,
 })
