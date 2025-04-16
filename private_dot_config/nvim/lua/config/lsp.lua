09f437c9ca6075ecbf8342af7a29234fd0a6a1b5
Nicolas Epstein <nilueps@gmail.com>
2025-04-16 11:58:17 -0400

    Update .config/nvim/lua/config/lsp.lua

--- private_dot_config/nvim/lua/config/lsp.lua
+++ private_dot_config/nvim/lua/config/lsp.lua
@@ -56,12 +56,12 @@ local servers = {
     volar = {
         init_options = {
             typescript = {
-                tsdk = vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib")
+                tsdk = vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib/")
             },
             vue = { hybridMode = true },
         },
         on_new_config = function(new_config, new_root_dir)
-            local lib_path = vim.fs.find(vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib"),
+            local lib_path = vim.fs.find(vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib/"),
                 { path = new_root_dir, upward = true })[1]
             if lib_path then
                 new_config.init_options.typescript.tsdk = lib_path
