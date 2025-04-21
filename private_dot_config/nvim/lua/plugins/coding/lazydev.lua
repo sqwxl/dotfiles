ff5ddf8b82478e0538edffe2abe467ae4a7528a9
Nicolas Epstein <nilueps@gmail.com>
2025-04-20 22:16:13 -0400

    Update .config/nvim/lua/plugins/coding/lazydev.lua

--- private_dot_config/nvim/lua/plugins/coding/lazydev.lua
+++ private_dot_config/nvim/lua/plugins/coding/lazydev.lua
@@ -1,16 +1,15 @@
-return {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    cmd = "LazyDev",
-    opts = {
-        library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            { path = "snacks.nvim",        words = { "Snacks" } },
-        },
-    },
-    enabled = function(root_dir)
-        return false
-        -- return root_dir ~= nil and (string.match(root_dir, "^" .. vim.fn.stdpath("config")) or
-        --     string.match(root_dir, "^" .. vim.fs.normalize("~/.local/share/chezmoi/private_dot_config/nvim")))
-    end,
-}
+-- return {
+--     "folke/lazydev.nvim",
+--     ft = "lua",
+--     cmd = "LazyDev",
+--     opts = {
+--         library = {
+--             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+--             { path = "snacks.nvim",        words = { "Snacks" } },
+--         },
+--     },
+--     enabled = function(root_dir)
+--         -- return root_dir ~= nil and (string.match(root_dir, "^" .. vim.fn.stdpath("config")) or
+--         --     string.match(root_dir, "^" .. vim.fs.normalize("~/.local/share/chezmoi/private_dot_config/nvim")))
+--     end,
+-- }
