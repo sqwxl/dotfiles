b477c59febd1a8a65d46ddd1123645dc43a23956
Nicolas Epstein <nilueps@gmail.com>
2025-04-20 22:17:44 -0400

    Update .config/nvim/lua/plugins/coding/lazydev.lua

--- private_dot_config/nvim/lua/plugins/coding/lazydev.lua
+++ private_dot_config/nvim/lua/plugins/coding/lazydev.lua
@@ -9,7 +9,6 @@ return {
         },
     },
     enabled = function(root_dir)
-        vim.notify(root_dir)
         return true
         -- return root_dir ~= nil and (string.match(root_dir, "^" .. vim.fn.stdpath("config")) or
         --     string.match(root_dir, "^" .. vim.fs.normalize("~/.local/share/chezmoi/private_dot_config/nvim")))
