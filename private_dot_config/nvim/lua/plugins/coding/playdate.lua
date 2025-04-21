480a97702233225482e5acd6498658282aae3375
Nicolas Epstein <nilueps@gmail.com>
2025-04-20 22:31:01 -0400

    Update .config/nvim/lua/plugins/coding/playdate.lua

--- private_dot_config/nvim/lua/plugins/coding/playdate.lua
+++ private_dot_config/nvim/lua/plugins/coding/playdate.lua
@@ -2,6 +2,9 @@ return {
     "sqwxl/playdate.nvim",
     opts = {
         playdate_luacats_path = "~/Clones/playdate-luacats",
+        build = {
+            source_dir = "Source",
+        },
         server_settings = {
             Lua = {
                 format = {
