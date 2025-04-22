9435c652a43f536d6f5dc2cbb391d439aa8ae93a
Nicolas Epstein <nilueps@gmail.com>
2025-04-22 10:00:13 -0400

    Update lazy-lock.json Update .config/nvim/lua/plugins/coding/lazydev.lua

--- private_dot_config/nvim/lua/plugins/coding/lazydev.lua
+++ private_dot_config/nvim/lua/plugins/coding/lazydev.lua
@@ -8,6 +8,9 @@ return {
             { path = "snacks.nvim",        words = { "Snacks" } },
         },
         enabled = function(root_dir)
+            if (vim.g.lazydev_enabled ~= nil and true) then
+                return true
+            end
             return root_dir ~= nil and (
                 string.match(root_dir, vim.fn.stdpath("config"))
                 or string.match(root_dir, vim.fs.normalize("~/.local/share/chezmoi/private_dot_config/nvim"))
