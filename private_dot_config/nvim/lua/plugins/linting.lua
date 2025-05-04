d0b33fa50f61432c256cf864ffe752646bfd1f5d
Nicolas Epstein <nilueps@gmail.com>
2025-05-04 16:06:06 -0400

    Update .config/nvim/lua/plugins/linting.lua

--- private_dot_config/nvim/lua/plugins/linting.lua
+++ private_dot_config/nvim/lua/plugins/linting.lua
@@ -3,6 +3,7 @@ return {
     opts = {
         events = { "BufWritePost", "BufReadPost", "InsertLeave" },
         linters_by_ft = {
+            text = nil,
             css = { "stylelint" },
             fish = { "fish" },
             html = { "htmlhint" },
