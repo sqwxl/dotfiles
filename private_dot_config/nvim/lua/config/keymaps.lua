8f389e9bf9fb1c503865f0c6eb7a9bdd96435c19
Nicolas Epstein <nilueps@gmail.com>
2025-05-25 21:20:29 -0400

    Update .config/nvim/lua/config/keymaps.lua

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -277,7 +277,7 @@ local keymaps = {
         {
             "<leader>cp",
             ft = "markdown",
-            "<cmd>MarkdownPreviewToggle<cr>",
+            "<cmd>MarkdownPreviewOpen<cr>",
             desc = "Markdown Preview",
         },
         { "<leader>cs", "<cmd>AerialToggle<cr>",                     desc = "Aerial (Symbols)" },
