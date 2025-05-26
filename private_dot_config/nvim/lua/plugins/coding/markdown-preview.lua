b338bbe5878bf279564ba23ce2360a48ca736c42
Nicolas Epstein <nilueps@gmail.com>
2025-05-25 20:42:06 -0400

    53abf099

--- private_dot_config/nvim/lua/plugins/coding/markdown-preview.lua
+++ private_dot_config/nvim/lua/plugins/coding/markdown-preview.lua
@@ -1,10 +1,11 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-        require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-        vim.fn["mkdp#util#install"]()
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
     config = function()
         vim.cmd([[do FileType]])
     end,
