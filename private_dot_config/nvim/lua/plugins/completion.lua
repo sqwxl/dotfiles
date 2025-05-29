3c0c716e73270f85efe9754002e8d9b1fe1b5ada
Nicolas Epstein <nilueps@gmail.com>
2025-05-29 09:11:18 -0400

    410eac5b38fafb3f

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -1,18 +1,22 @@
 return {
     "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
     version = "1.*",
     opts_extend = { "sources.default" },
-    dependencies = { "rafamadriz/friendly-snippets" },
     event = "InsertEnter",
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
         keymap = { preset = "enter" },
+
         appearance = { nerd_font_variant = "normal" },
+
         fuzzy = { implementation = "rust" },
+
         completion = {
             menu = {
+                max_height = 30,
                 draw = {
                     treesitter = { "lsp" },
                 },
