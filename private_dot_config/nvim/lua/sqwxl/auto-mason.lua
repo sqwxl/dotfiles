7266586724eeb62785364aa1bad00e2f14cf6626
Nicolas Epstein <nilueps@gmail.com>
2025-06-09 09:57:13 -0400

    Update .config/nvim/lua/sqwxl/auto-mason.lua

--- private_dot_config/nvim/lua/sqwxl/auto-mason.lua
+++ private_dot_config/nvim/lua/sqwxl/auto-mason.lua
@@ -58,7 +58,12 @@ end
 local function get_lint_packages()
 	local linters_by_ft = require("lint").linters_by_ft
 
-	return vim.iter(linters_by_ft):flatten():totable()
+	return vim.iter(linters_by_ft)
+		:map(function(_, v)
+			return v
+		end)
+		:flatten()
+		:totable()
 end
 
 local function get_lsp_packages()
