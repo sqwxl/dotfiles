14ce346f880d597f07b98d89ccacdd4e2c1f4909
Nicolas Epstein <nilueps@gmail.com>
2025-06-15 19:05:38 -0400

    Update .config/nvim/lua/plugins/formatting.lua

--- private_dot_config/nvim/lua/plugins/formatting.lua
+++ private_dot_config/nvim/lua/plugins/formatting.lua
@@ -28,7 +28,7 @@ return {
 			javascriptreact = { "biome-check" },
 			json = { "biome-check" },
 			jsonc = { "biome-check" },
-			ruby = { "standardrb" },
+			-- ruby = { "standardrb" },
 			eruby = { "erb_format" },
 			terraform = { "terraform_fmt" },
 			tf = { "terraform_fmt" },
