9f4b688b3232a85ddff1e3b863424cdd8496fc94
Nicolas Epstein <nilueps@gmail.com>
2025-06-30 18:45:43 -0400

    Update .config/foot/foot.ini Update .config/nvim/lua/plugins/editor/snacks.lua

--- private_dot_config/nvim/lua/plugins/editor/snacks.lua
+++ private_dot_config/nvim/lua/plugins/editor/snacks.lua
@@ -1,81 +1,103 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-        bigfile = { enabled = true },
-        dashboard = {
-            enabled = true,
-            preset = {
-                header = [[
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			enabled = true,
+			preset = {
+				header = [[
         ______ _____ ______ __  __ __ __________
        /\  __ \\  __\\  __ \\ \/\ \\_\\  __  __ \
        \ \ \/\ \\  __\\ \_\ \\ \/ |/\ \\ \/\ \/\ \
         \ \_\ \_\\____\\_____\\__/ \ \_\\_\ \_\ \_\
          \/_/\/_//____//_____//_/   \/_//_/\/_/\/_/]],
-                keys = {
-                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                    { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                    { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-                    { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-                    { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-                    { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                }
-            },
-            formats = {
-                header = { "%s", align = "left" },
-            },
-        },
-        notifier = { enabled = false, timeout = 3000 },
-        statuscolumn = { enabled = true },
-        picker = { enabled = true },
-        styles = { notifications = { wo = { wrap = true }, relative = true } },
-    },
-    keys = {
-    },
-    init = function()
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "VeryLazy",
-            callback = function()
-                -- Setup some globals for debugging (lazy-loaded)
-                _G.dd = function(...)
-                    Snacks.debug.inspect(...)
-                end
-                _G.bt = function()
-                    Snacks.debug.backtrace()
-                end
-                vim.print = _G.dd -- Override print to use snacks for `:=` command
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = "󰙅 ", key = "e", desc = "Explorer", action = ":Neotree" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{ icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+					{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+			},
+			formats = {
+				header = { "%s", align = "left" },
+			},
+		},
+		notifier = { enabled = false, timeout = 3000 },
+		statuscolumn = { enabled = true },
+		picker = { enabled = true },
+		styles = { notifications = { wo = { wrap = true }, relative = true } },
+	},
+	keys = {},
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- Setup some globals for debugging (lazy-loaded)
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+				vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-                -- Create some toggle mappings
-                Snacks.toggle.zoom():map("<leader>wm"):map("<leader>uZ")
-                Snacks.toggle.zen():map("<leader>uz")
-                Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-                Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-                Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-                Snacks.toggle.diagnostics():map("<leader>ud")
-                Snacks.toggle.line_number():map("<leader>ul")
-                Snacks.toggle.option("conceallevel",
-                    { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, name = "Conceal Level" }):map(
-                    "<leader>uc")
-                Snacks.toggle.option("showtabline",
-                    { off = 0, on = vim.o.showtabline > 0 and vim.o.showtabline or 2, name = "Tabline" })
-                    :map("<leader>uA")
-                Snacks.toggle.treesitter():map("<leader>uT")
-                Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map(
-                    "<leader>ub")
-                Snacks.toggle.dim():map("<leader>uD")
-                Snacks.toggle.animate():map("<leader>ua")
-                Snacks.toggle.indent():map("<leader>ug")
-                Snacks.toggle.scroll():map("<leader>uS")
-                Snacks.toggle.profiler():map("<leader>dpp")
-                Snacks.toggle.profiler_highlights():map("<leader>dph")
+				-- Create some toggle mappings
+				Snacks.toggle.zoom():map("<leader>wm"):map("<leader>uZ")
+				Snacks.toggle.zen():map("<leader>uz")
+				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+				Snacks.toggle.diagnostics():map("<leader>ud")
+				Snacks.toggle.line_number():map("<leader>ul")
+				Snacks.toggle
+					.option(
+						"conceallevel",
+						{ off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, name = "Conceal Level" }
+					)
+					:map("<leader>uc")
+				Snacks.toggle
+					.option(
+						"showtabline",
+						{ off = 0, on = vim.o.showtabline > 0 and vim.o.showtabline or 2, name = "Tabline" }
+					)
+					:map("<leader>uA")
+				Snacks.toggle.treesitter():map("<leader>uT")
+				Snacks.toggle
+					.option("background", { off = "light", on = "dark", name = "Dark Background" })
+					:map("<leader>ub")
+				Snacks.toggle.dim():map("<leader>uD")
+				Snacks.toggle.animate():map("<leader>ua")
+				Snacks.toggle.indent():map("<leader>ug")
+				Snacks.toggle.scroll():map("<leader>uS")
+				Snacks.toggle.profiler():map("<leader>dpp")
+				Snacks.toggle.profiler_highlights():map("<leader>dph")
 
-                Snacks.toggle.inlay_hints():map("<leader>uh")
-            end,
-        })
-    end,
+				Snacks.toggle.inlay_hints():map("<leader>uh")
+			end,
+		})
+	end,
 }
