5b596bae78a91497f71554d958dcb0db29233a83
Nicolas Epstein <nicolas.epstein@sterlingcapitalbrokers.com>
2025-07-02 21:24:17 -0400

    Update .config/nvim/lua/config/lsp.lua

--- private_dot_config/nvim/lua/config/lsp.lua
+++ private_dot_config/nvim/lua/config/lsp.lua
@@ -36,18 +36,18 @@ local servers = {
 	-- JavaScript
 	ts_ls = {
 		init_options = {
-			plugins = {
-				{
-					name = "@vue/typescript-plugin",
-					location = vim.fs.joinpath(vim.g.npm_global_modules, "@vue/typescript-plugin"),
-					languages = { "javascript", "typescript", "vue" },
-				},
-			},
+			-- plugins = {
+			-- 	{
+			-- 		name = "@vue/typescript-plugin",
+			-- 		location = vim.fs.joinpath(vim.g.npm_global_modules, "@vue/typescript-plugin"),
+			-- 		languages = { "javascript", "typescript", "vue" },
+			-- 	},
+			-- },
 		},
 		filetypes = {
 			"javascript",
 			"typescript",
-			"vue",
+			-- "vue",
 		},
 	},
 
