79027e82a724094ccff495ce29ba0a61368c05a0
Nicolas Epstein <nicolas.epstein@sterlingcapitalbrokers.com>
2025-07-02 22:29:44 -0400

    Update lazy-lock.json Add .config/ghostty/config Update .config/nvim/lua/config/lsp.lua

--- private_dot_config/nvim/lua/config/lsp.lua
+++ private_dot_config/nvim/lua/config/lsp.lua
@@ -65,23 +65,23 @@ local servers = {
 	terraformls = {},
 
 	-- Vue
-	vue_ls = {
-		init_options = {
-			typescript = {
-				tsdk = vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib/"),
-			},
-			vue = { hybridMode = true },
-		},
-		on_new_config = function(new_config, new_root_dir)
-			local lib_path = vim.fs.find(
-				vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib/"),
-				{ path = new_root_dir, upward = true }
-			)[1]
-			if lib_path then
-				new_config.init_options.typescript.tsdk = lib_path
-			end
-		end,
-	},
+	-- vue_ls = {
+	-- 	init_options = {
+	-- 		typescript = {
+	-- 			tsdk = vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib/"),
+	-- 		},
+	-- 		vue = { hybridMode = true },
+	-- 	},
+	-- 	on_new_config = function(new_config, new_root_dir)
+	-- 		local lib_path = vim.fs.find(
+	-- 			vim.fs.joinpath(vim.g.npm_global_modules, "typescript/lib/"),
+	-- 			{ path = new_root_dir, upward = true }
+	-- 		)[1]
+	-- 		if lib_path then
+	-- 			new_config.init_options.typescript.tsdk = lib_path
+	-- 		end
+	-- 	end,
+	-- },
 
 	-- HTML
 	html = {
