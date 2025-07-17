19d4aabe243a255af04a73f28e48f05d9d273cd2
Nicolas Epstein <nicolas.epstein@sterlingcapitalbrokers.com>
2025-07-17 17:09:32 -0400

    Update .config/ghostty/config Update .config/nvim/lua/plugins/lang/typescript.lua

--- private_dot_config/nvim/lua/plugins/lang/typescript.lua
+++ private_dot_config/nvim/lua/plugins/lang/typescript.lua
@@ -29,7 +29,7 @@ return {
 		opts = function(_, opts)
 			for _, ft in ipairs(fts) do
 				opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
-				table.insert(opts.linters_by_ft[ft], "biome-check")
+				table.insert(opts.linters_by_ft[ft], "biomejs")
 			end
 		end,
 	},
