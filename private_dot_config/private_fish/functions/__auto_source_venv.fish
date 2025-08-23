14dd53c9580b873f0625f9fde9dc9a0b3adba70b
Nicolas Epstein <nicolas.epstein@sterlingcapitalbrokers.com>
2025-08-23 14:34:25 -0400

    Update .config/fish/functions/__auto_source_venv.fish

--- private_dot_config/private_fish/functions/__auto_source_venv.fish
+++ private_dot_config/private_fish/functions/__auto_source_venv.fish
@@ -1,21 +1,28 @@
 function __auto_source_venv --description 'Activate/Deactivate virtualenv on directory change' --on-variable PWD
-    status is-command-substitution; and return
+    status is-command-substitution; and return 0
 
-    # Check if we are inside a git directory
-    if git rev-parse --show-toplevel &>/dev/null
-        set gitdir (realpath (git rev-parse --show-toplevel))
-    else
-        set gitdir "."
+    set dir (pwd)
+    set gitdir
+    set venv
+
+    git rev-parse --is-inside-work-tree &>/dev/null; and set gitdir (git rev-parse --show-toplevel)
+
+    if test -d "$dir/.venv"
+        set venv "$dir/.venv"
+    else if test -d "$gitdir/.venv"
+        set venv "$gitdir/.venv"
     end
 
-    # If venv is not activated or a different venv is activated and venv exists.
-    if test "$VIRTUAL_ENV" != "$gitdir/.venv" -a -e "$gitdir/.venv/bin/activate.fish"
-        if type -q deactivate
-            deactivate
-        end
-        source "$gitdir/.venv/bin/activate.fish"
-        # If venv activated but the current (git) dir has no venv.
-    else if test -n "$VIRTUAL_ENV" -a ! -d "$gitdir/.venv" && type -q deactivate
+    if test -z "$venv"; and type -q deactivate
         deactivate
+        return 0
     end
+
+    if test "$VIRTUAL_ENV" != "$venv" -a -e "$venv/bin/activate.fish"
+        type -q deactivate; and deactivate
+
+        source "$venv/bin/activate.fish"
+        return 0
+    end
+
 end
