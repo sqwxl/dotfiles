6311b1a273cb40e9e068a9ace2df957141b1c62a
Nicolas Epstein <nilueps@gmail.com>
2025-09-24 08:27:00 -0400

    Update .config/nvim/lua/plugins/lang/typescript.lua

--- private_dot_config/nvim/lua/plugins/lang/typescript.lua
+++ private_dot_config/nvim/lua/plugins/lang/typescript.lua
@@ -23,16 +23,16 @@ return {
 		},
 	},
 
-	{
-		"mfussenegger/nvim-lint",
-		optional = true,
-		opts = function(_, opts)
-			for _, ft in ipairs(fts) do
-				opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
-				table.insert(opts.linters_by_ft[ft], "biomejs")
-			end
-		end,
-	},
+	-- {
+	-- 	"mfussenegger/nvim-lint",
+	-- 	optional = true,
+	-- 	opts = function(_, opts)
+	-- 		for _, ft in ipairs(fts) do
+	-- 			opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
+	-- 			table.insert(opts.linters_by_ft[ft], "biomejs")
+	-- 		end
+	-- 	end,
+	-- },
 
 	{
 		"stevearc/conform.nvim",
