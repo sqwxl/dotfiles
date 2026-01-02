f38dc4b5c6a884f7a961528900ecc782f7643da3
Nicolas Epstein <nilueps@gmail.com>
2026-01-02 11:05:18 -0500

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,92 +1,81 @@
-abbr n nvim
+alias n nvim
 set -gx EDITOR (which nvim)
 set -gx VISUAL $EDITOR
 
-abbr devim 'devcontainer exec --workspace-folder . nvim'
-
-alias cz chezmoi
+abbr devim "devcontainer exec --workspace-folder . nvim"
 
 abbr s sudo
 abbr se sudoedit
 
-abbr dc 'docker compose'
+abbr dc "docker compose"
 
 abbr rm "rm -v"
 abbr mv "mv -iv"
 abbr cp "cp -riv"
 abbr mkdir "mkdir -vp"
 
-abbr czsway "cz edit --apply ~/.config/sway/config"
-abbr czvim "cz edit --apply ~/.config/nvim/lua"
-abbr czfish "cz edit --apply ~/.config/fish/config.fish"
-abbr czterm "cz edit --apply ~/.config/foot/foot.ini"
-abbr czcommit "cz git commit -- -am (openssl rand -hex 4) && cz git push"
-
 # Weather in Stukely
 abbr weather 'curl -s "wttr.in/45.32,-72.42?nFQ"'
+
+if command -q chezmoi
+    alias cz chezmoi
+    abbr czsway "cz edit --apply ~/.config/sway/config"
+    abbr czvim "cz edit --apply ~/.config/nvim/lua"
+    abbr czfish "cz edit --apply ~/.config/fish/config.fish"
+    abbr czterm "cz edit --apply ~/.config/foot/foot.ini"
+end
 
 if test "$TERM" = foot -o "$TERM" = foot-extra
     abbr ssh "TERM=linux command ssh"
 end
 
-abbr l ls
-abbr ll 'ls --long --group-directories-first'
-abbr lll 'ls --all --header --long'
-
-if type -q eza
+if command -q eza
     alias ls eza
-    alias ll 'eza --icons=auto --long --group-directories-first'
-    alias l. 'eza --treat-dirs-as-files --group-directories-first .*'
-    alias l1 'eza --oneline'
+    alias l eza
+    alias l. "eza --treat-dirs-as-files --group-directories-first .*"
+    alias l1 "eza --oneline"
+    alias ll "eza --icons=auto --long --group-directories-first"
+    alias lll "eza --icons=auto --all --header --long --group-directorie-first"
 end
 
-if type -q fzf
+if command -q fzf
     fzf --fish | source
+    if command -q fd
+        set -gx FZF_DEFAULT_COMMAND fd --type file --strip-cwd-prefix --hidden --follow --exclude .git
+        set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+    end
+    command -q bat; and set -gx FZF_CTRL_T_OPTS --preview '"bat --color=always --style=numbers --line-range=:500 {}"'
 end
 
-if type -q fd
-    set -gx FZF_DEFAULT_COMMAND fd --type file
-    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-end
-
-if type -q bat
+if command -q bat
     alias cat bat
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-    set -gx MANROFFOPT -c
+    abbr --position anywhere -- --help "--help | bat --plain --language help"
+    set -gx MANPAGER "bat --plain --language man"
     set -gx BAT_THEME gruvbox-dark
-    set -gx FZF_CTRL_T_OPTS --preview '"bat --color=always --style=numbers --line-range=:500 {}"'
 end
 
-if type -q go
+if command -q go
     if test -z "$GOPATH"
-        set -gx GOPATH "$HOME/go"
+        set -gx GOPATH $HOME/go
     end
     fish_add_path $GOPATH/bin
 end
 
-function prompt_nl --on-event fish_postexec
-    echo
-end
+command -q direnv; and direnv hook fish | source
 
-if type -q direnv
-    direnv hook fish | source
-end
-
-if type -q fnm
+if command -q fnm
     fnm env --use-on-cd --corepack-enabled --version-file-strategy=recursive --shell fish | source
 end
 
-source "$__fish_config_dir/functions/__auto_source_venv.fish"
-
-# Added by `rbenv init` on Tue 24 Jun 2025 10:38:38 EDT
-status --is-interactive; and rbenv init - --no-rehash fish | source
+if status is-interactive
+    command -q starship; and starship init fish | source
+    command -q zoxide; and zoxide init fish | source
+    command -q rbenv; and rbenv init - --no-rehash fish | source
+    alias zz "cd -"
+end
 
 fish_add_path --prepend --move $HOME/.npm-global/bin
 fish_add_path --prepend --move $HOME/.cargo/bin
 fish_add_path --prepend --move $HOME/.local/bin
 
-if status is-interactive
-    type -q starship && starship init fish | source
-    type -q zoxide && zoxide init fish | source
-    alias zz 'cd -'
-end
+source "$__fish_config_dir/functions/__auto_source_venv.fish"
