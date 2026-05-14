7c4d2aa09855d558776f3ee87b081b2fb4c84381
Nicolas Epstein <nilueps@gmail.com>
2026-05-14 07:49:36 -0400

    Update .config/nvim/lua/plugins/lang/sql.lua

--- private_dot_config/nvim/lua/plugins/lang/sql.lua
+++ private_dot_config/nvim/lua/plugins/lang/sql.lua
@@ -28,14 +28,14 @@ return {
 		"stevearc/conform.nvim",
 		optional = true,
 		opts = function(_, opts)
-			opts.formatters.sqlfluff = {
-				args = { "format", "--dialect=postgres", "-" },
-				require_cwd = false,
-			}
 			for _, ft in ipairs(sql_ft) do
 				opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
 				table.insert(opts.formatters_by_ft[ft], "sqlfluff")
 			end
+			opts.formatters.sqlfluff = {
+				args = { "format", "--dialect=postgres", "-" },
+				require_cwd = false,
+			}
 		end,
 	},
 
